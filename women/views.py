5d8d5d9ccecd772b39c762f1ce7bd98b98f547d0
ilgamkhisam <ilgamkhisam@gmail.com>
2023-11-29 18:53:15 +0600

    Lessons12

--- women/views.py
+++ women/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect 
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import Women, Category
 
@@ -8,27 +8,35 @@ from .models import Women, Category
 
 # menu было реализовано при помощи пользовательского тега 
 
-def show_post(request, post_id): # функции заглушки для вывода страниц
-    return HttpResponse(f'{post_id}-ID статьи')
+def show_post(request, post_slug): # функции заглушки для вывода страниц
+    post = get_object_or_404(Women, slug=post_slug)
+
+    context = {
+        'post': post,
+        'title': post.title, 
+        'cat_selected': post.cat_id, 
+    }
+
+    return render(request,'women/post.html', context=context) 
+
 
 # функция вывода постов выбранной категории, 
 # где передается id выбранной каторегории
 # и на основе нее создается sql запрос для фильтрации 
 
-def show_category(request, cat_id): 
-    posts = Women.objects.filter(cat_id=cat_id) # запрос в базу с фильтрацией и учетом id категории 
+def show_category(request, cat_slug): 
+    category = get_object_or_404(Category, slug=cat_slug) # запрос на получения категории, 
+
+    posts = Women.objects.filter(cat_id=category.pk) # запрос в базу с фильтрацией и учетом id категории 
     #cats = Category.objects.all()   
-    
     # проверка на наличие постов в выбранной категории, 
     # если их нет то выдает ошибку page_not_found_404
-    if len(posts) == 0: 
-        raise Http404()
 
     # создание переменной для передачи информации
     context = {
         'posts':posts,
         'title':'Отображение по рубрикам',
-        'cat_selected': cat_id,
+        'cat_selected': category.pk,
     }
     return render(request, 'women/index.html', context=context)
 
